c832ebb7b88633255904df1ebec97c1f30e30679
Tyler Reece <tyler_reece@outlook.com>
2018-05-17 20:59:56 -0400

    Initiated project with spring initializer and created .gitignore

---
+++ src/main/java/com/riot/leaguealytics/LeaguealyticsApplication.java
@@ -0,0 +1,12 @@
+package com.riot.leaguealytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaguealyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaguealyticsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/riot/leaguealytics/LeaguealyticsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.riot.leaguealytics;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LeaguealyticsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
